5287b350301cbf1c4e57761421ae4b634dfaeb7d
Melvin Thomas <melvinchooranolil@gmail.com>
2022-09-14 00:10:34 +0530

    base models

--- baseapp/models.py
+++ baseapp/models.py
@@ -1,3 +1,23 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class UUIDModel(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    class Meta:
+        abstract = True
+
+
+class TimestampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class TimestampedUUIDModel(UUIDModel, TimestampedModel):
+    class Meta:
+        abstract = True
